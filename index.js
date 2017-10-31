c42dc20b9ea1cc02a80b2fae66934e4359b99569
Robert Mennell <skatcat31@gmail.com>
2017-10-30 18:26:29 -0700

    MicroModel, the bad version
    this is an insecure proof of concept

---
+++ index.js
@@ -0,0 +1,20 @@
+class MicroModel{
+  constructor(props){
+    this.node = document.getElementById(props.id);
+    this.state = {};
+    if(this.render){
+      window.setTimeout(this.render.bind(this), 0);
+    }
+  }
+
+  setState(state){
+    if( this.state != state && Object.values(state).length ){
+      Object.assign(this.state, state);
+      if(this.render){
+        this.render();
+      }
+    }
+  }
+}
+
+// export default MicroModel;
