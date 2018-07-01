9c4e2ada53490804085c3265d616d7f8ccc14cd7
Robert Mennell <skatcat31@users.noreply.github.com>
2018-07-01 11:23:01 -0700

    update to requestAnimationFrame instead of timeout

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ class MicroModel{
     this.node = document.getElementById(props.id);
     if(this.render){
       this.render = this.render.bind(this);
-      window.setTimeout(this.render, 0);
+      window.requestAnimationFrame(this.render);
     }
   }
 
@@ -11,7 +11,7 @@ class MicroModel{
     if( this.state != state && Object.values(state).length ){
       this.state = Object.assign({}, this.state, state);
       if(this.render){
-        window.setTimeout(this.render, 0);
+        window.requestAnimationFrame(this.render);
       }
     }
   }
